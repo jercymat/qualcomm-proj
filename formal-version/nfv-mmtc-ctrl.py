133773d62331a9c0d39d8aa3211d48323038ae25
Han-Hsuan Lin <105703004@nccu.edu.tw>
2019-10-29 16:41:52 +0800

    feat: add REST server and ARP handling

--- formal-version/nfv-mmtc-ctrl.py
+++ formal-version/nfv-mmtc-ctrl.py
@@ -4,16 +4,27 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import ethernet, ether_types, arp
+
+# REST Api Packages
+import json
+import logging
+from ryu.app.wsgi import WSGIApplication, ControllerBase, route
+from webob import Response
+
+ue_instance_name = 'NFV_mMTC'
 
 
 class NFVmmtcCtrl(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {'wsgi': WSGIApplication}
 
     def __init__(self, *args, **kwargs):
         super(NFVmmtcCtrl, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        self.mac_to_port = {}  # mac address to inbound port table
+        self.arp_broadcast = {}  # arp broadcast inbound port table
+        wsgi = kwargs['wsgi']
+        wsgi.register(EastWestAPI, {ue_instance_name: self})
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -63,18 +74,33 @@ class NFVmmtcCtrl(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
         dst = eth.dst
         src = eth.src
-
         dpid = datapath.id
+
+        # ignore lldp and ipv6 packet
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP or eth.ethertype == ether_types.ETH_TYPE_IPV6:
+            return
+
+        self.logger.info("Packet In: %s %s -> %s | port:%s", dpid, src, dst, in_port)
+
+        if eth.ethertype == ether_types.ETH_TYPE_ARP and dst == "ff:ff:ff:ff:ff:ff":
+            arp_dst_ip = pkt.get_protocol(arp.arp).dst_ip
+            if (dpid, src, arp_dst_ip) in self.arp_broadcast:
+                if self.arp_broadcast[(dpid, src, arp_dst_ip)] != in_port:
+                    out = datapath.ofproto_parser.OFPPacketOut(
+                        datapath=datapath,
+                        buffer_id=datapath.ofproto.OFP_NO_BUFFER,
+                        in_port=in_port,
+                        actions=[], data=None)
+                    datapath.send_msg(out)
+                    self.logger.info('ARP Loop Maker Dropped!')
+
+                    return
+            else:  # learn arp broadcast table
+                self.arp_broadcast[(dpid, src, arp_dst_ip)] = in_port
+
         self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
@@ -102,3 +128,53 @@ class NFVmmtcCtrl(app_manager.RyuApp):
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+
+class EastWestAPI(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(EastWestAPI, self).__init__(req, link, data, **config)
+        self.mMTC_app = data[ue_instance_name]
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.authentication = {
+            'TEST': '033bd94b1168d7e4f0d644c3c95e35bf',
+            'URLLC': 'dfdc323ef40c8f38f5f022a5554f2dee',
+            'EMBB': 'e9406f9b456569f230a23d21464189f2',
+            'MMTC': '1d99331a7879c3c5b90998f8c3c6a9d6'
+        }
+        self.unauthenticated = Response(
+            status=401,
+            content_type='application/json',
+            body=json.dumps(
+                {'error': 'unauthenticated'},
+                indent=4
+            ) + '\n'
+        )
+
+    @route('mac-port-table', '/mac-port-table', methods=['GET'])
+    def _mac_port_table(self, req, **kwargs):
+        mMTC_app = self.mMTC_app
+        body = json.dumps(mMTC_app.mac_to_port, indent=4) + '\n'
+
+        if 'Authentication' in req.headers:
+            if req.headers['Authentication'] in self.authentication.values():
+                return Response(content_type='application/json', body=body)
+            else:
+                return self.unauthenticated
+        else:
+            return self.unauthenticated
+
+    @route('arp-table', '/arp-table', methods=['GET'])
+    def _arp_table(self, req, **kwargs):
+        arp_broadcast = self.mMTC_app.arp_broadcast
+        arp_str_table = {}
+        for key in arp_broadcast.keys():
+            arp_str_table[str(key)] = arp_broadcast[key]
+        body = json.dumps(arp_str_table, indent=4) + '\n'
+
+        if 'Authentication' in req.headers:
+            if req.headers['Authentication'] in self.authentication.values():
+                return Response(content_type='application/json', body=body)
+            else:
+                return self.unauthenticated
+        else:
+            return self.unauthenticated
